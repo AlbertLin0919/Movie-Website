60c39c347f29d5e57d03f7004616ce80b26a6b40
Albert Lin <albert88919@yahoo.com.tw>
2023-03-31 15:45:18 +0800

    設定basename在BrowserRouter裡

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Movie-Website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
